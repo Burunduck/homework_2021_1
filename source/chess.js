4d96a7a7d2b46e49fbbd5aea849261ed3da767d0
Dima Akzhigitov <trololoshka609@gmail.com>
2021-02-09 13:28:58 +0300

    Second variant solutione

--- source/chess.js
+++ source/chess.js
@@ -1,23 +1,20 @@
 'use strict';
 
-function addLine(board, black, white, n, nextBlack) {
-    for (let j = 0; j < n; j++) {
-        board += nextBlack ? black : white;
-        nextBlack = !nextBlack;
-    }
-    return board + '\n';
-}
-
 function chess(n) {
     if (n <= 1) return null;
     const white = ' ';
     const black = '*';
-    let nextBlack = true;
-    let board = '';
-    for (let i = 0; i < n; i++) {
-        board = nextBlack ? addLine(board, black, white, n, true) :
-            addLine(board, black, white, n, false);
+    let nextBlack = false;
+    let counter = 1;
+    n = +n;
+    let board = ' '.repeat(n * n + n).split('').map(() => {
         nextBlack = !nextBlack;
-    }
+        if ( !(counter++ % (n+1))) {
+            nextBlack = !nextBlack;
+            if (!(n%2)) nextBlack = !nextBlack;
+            return '\n';
+        }
+        return nextBlack ? black : white;
+    }).join('');
     return board;
 }
