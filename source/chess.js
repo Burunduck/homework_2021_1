efdd7cbe9d8393807fbcf35266aa705fc99c31fb
Dima Akzhigitov <trololoshka609@gmail.com>
2021-02-09 16:11:43 +0300

    Review 1

--- source/chess.js
+++ source/chess.js
@@ -1,20 +1,49 @@
 'use strict';
 
 function chess(n) {
-    if (n <= 1) return null;
+
+    if (n <= 1) {
+        return null;
+    }
+
+    /** @constant {string}
+     * @description Char for white color cell
+     */
     const white = ' ';
+
+    /**
+     * @constant {string}
+     * @description Char for black color cell
+     */
     const black = '*';
-    let nextBlack = false;
-    let counter = 1;
-    n = +n;
-    let board = ' '.repeat(n * n + n).split('').map(() => {
-        nextBlack = !nextBlack;
-        if ( !(counter++ % (n+1))) {
-            nextBlack = !nextBlack;
-            if (!(n%2)) nextBlack = !nextBlack;
-            return '\n';
+
+    /**
+     * @description If true - means that last cell color was black, else last cell color was white
+     */
+    let currentCell = true;
+
+
+    /**
+     * @description Chess board at string format
+     */
+    let board = '';
+
+    /**
+     * @description Add one line to the chess board.
+     * @param {boolean} currentCell - From what color cell new line have to start.
+     * @returns {string} chess board with new line
+     */
+    let addLine = (currentCell) => {
+        for (let j = 0; j < n; j++) {
+            board += currentCell ? black : white;
+            currentCell = !currentCell;
         }
-        return nextBlack ? black : white;
-    }).join('');
+        return board + '\n';
+    }
+
+    for (let i = 0; i < n; i++) {
+        board = addLine(currentCell);
+        currentCell = !currentCell;
+    }
     return board;
 }
